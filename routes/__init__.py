31df431b5ee9b018986fd2b0d9785e97ec5ef0c2
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-10-21 10:29:50 +0100

    Update project structure

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python3
 from flask import Blueprint
 
 frontend = Blueprint('frontend', __name__, url_prefix='/')
 
 from routes.signup import *
-from routes.verify import *
 from routes.utils import *
 from routes.login import *
