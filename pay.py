be3257612d9685d985a594d0be6cfab2114b9252
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-10-29 17:28:49 +0100

    Update paystack payment route

--- pay.py
+++ pay.py
@@ -1,16 +1,17 @@
 import requests
-from os import getenv
 import secrets
+from os import getenv
+
 paystack_key = getenv('PAYSTACK_KEY')
 
-def initialize_transaction(amount, project_id, user_id):
+def initialize_and_verify_transaction(amount, project_id, user_id):
+    # Initialize the transaction
     url = "https://api.paystack.co/transaction/initialize"
     headers = {
         "Authorization": f"Bearer {paystack_key}",
         "Content-Type": "application/json"
     }
     
-    # Generate a unique reference by including user and project information
     reference = f'project_{project_id}_user_{user_id}_payment'
     
     data = {
@@ -28,16 +29,26 @@ def initialize_transaction(amount, project_id, user_id):
     response = requests.post(url, headers=headers, json=data)
 
     if response.status_code == 200:
-        # Successful response
         result = response.json()
-        # return result['data']['authorization_url']
-        return result
-    
+        reference = result['data']['reference']
+        
+        # Verify the transaction
+        verify_url = f"https://api.paystack.co/transaction/verify/{reference}"
+        verify_headers = {
+            "Authorization": f"Bearer {paystack_key}",
+        }
+        
+        verify_response = requests.get(verify_url, headers=verify_headers)
+
+        if verify_response.status_code == 200:
+            return verify_response.json()['data']['status']
+        else:
+            return f"Error: {verify_response.status_code} - {verify_response.text}"
     else:
-        # Handle the error or return an error message
         return f"Error: {response.status_code} - {response.text}"
 
 # Call the function with user-specific data
 user_id = secrets.token_hex(6)
-response_data = initialize_transaction(100, 100, user_id)
+response_data = initialize_and_verify_transaction(100, 100, user_id)
 print(response_data)
+
