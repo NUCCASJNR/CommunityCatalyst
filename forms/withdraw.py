6eb66d38e0262672c197b9bef7eaefc66bdf993a
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-11-02 21:47:08 +0100

    Update Withdrawal form

--- forms/withdraw.py
+++ forms/withdraw.py
@@ -11,6 +11,9 @@ class WithdrawalForm(FlaskForm):
     Withdrawal form that inherits from flask form
     """
     campaign_name = StringField('Campaign Name', validators=[DataRequired(), Length(max=126)])
+    acc_name = StringField('Account Name', validators=[DataRequired(), Length(max=126)])
+    acc_number = StringField('Account Number', validators=[DataRequired(), Length(max=126)])
+    bank = StringField('Bank', validators=[DataRequired(), Length(max=126)])
     amount = DecimalField('Amount', validators=[DataRequired()])
     project_id = StringField('Project_id', validators=[DataRequired()])
     request_payment = SubmitField('Reqeust Payment')
