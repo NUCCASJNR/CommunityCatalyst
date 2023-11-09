d4b7dccd74bdd9f651b9ec118533852134501975
Al-Areef <113596908+NUCCASJNR@users.noreply.github.com>
2023-11-09 09:28:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import jsonify, Response, session
-from models.base_model import app
+from flask import jsonify, Response, render_template, session
+from models.base_model import app, db  # Assuming 'db' is your SQLAlchemy instance
 from models.user import User
 from flask_bcrypt import Bcrypt
 from flask_wtf.csrf import CSRFProtect
 from models.project import Project
 from flask_login import LoginManager
-
 from flask_cors import CORS
 from routes import frontend
 import logging
@@ -14,10 +13,8 @@ from utils.redis_client import RedisClient
 
 app.register_blueprint(frontend)
 csrf = CSRFProtect(app)
-# Enable SQL Alchemy query logging
 logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
-
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
@@ -25,20 +22,21 @@ login_manager.login_message_category = 'info'
 
 bcrypt = Bcrypt(app)
 
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(str(user_id))
 
+# Add this line to create tables
+with app.app_context():
+    db.create_all()
 
 @app.errorhandler(404)
 def error(error) -> Response:
     return jsonify({"error": "Not found"})
 
-
 @app.errorhandler(403)
 def forbidden_err(error):
-    return jsonify({"error": "unauthorized"})
+    return jsonify({"error": "Unauthorized"})
 
 @app.route("/")
 def index():
@@ -47,12 +45,10 @@ def index():
 
 @app.route('/token', methods=['GET'])
 def token():
-    # Generate a CSRF token and store it in the session
     csrf_token = generate_csrf()
     session['csrf_token'] = csrf_token
-    session.modified = True  # Ensure the session is saved
+    session.modified = True
     return jsonify({'X-CSRFToken': csrf_token})
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
